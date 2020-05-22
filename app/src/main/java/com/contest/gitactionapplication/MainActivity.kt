7909574e9786014b7c24782f40c8a4dc93a031ed
manish <manish.kumar@tothenew.com>
2020-05-22 18:38:16 +0530

    test commit

--- app/src/main/java/com/contest/gitactionapplication/MainActivity.kt
+++ app/src/main/java/com/contest/gitactionapplication/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MANISH"," First logger")
+        Log.d("MANISH"," First logger2222")
 
 
 
